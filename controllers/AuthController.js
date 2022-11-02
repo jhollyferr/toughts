e43095ba8261cf917479dc536b821e01fda1134f
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-11-02 05:30:33 -0300

    :sparkles: define created user session

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -29,6 +29,32 @@ module.exports = class AuthController {
         return;
       }
 
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        request.flash("message", "O e-mail já está em uso");
+        response.render("auth/register");
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      if (user) {
+        request.flash("message", "Cadastro realizado com sucesso");
+
+        request.session.userId = user.id;
+
+        request.session.save(() => {
+          response.redirect("/");
+        });
+      }
     } catch (error) {
       console.log(error);
     }
